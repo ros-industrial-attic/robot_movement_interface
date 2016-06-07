2abb448a91c45cb9c731b9ae650cf5bc2a01d6c0
Pablo Quílez <pablo-quilez@users.noreply.github.com>
2016-06-07 16:26:42 +0200

    IOs corrected also for ur-devel-tf branch

--- ur_driver/src/connector.cpp
+++ ur_driver/src/connector.cpp
@@ -385,8 +385,8 @@ void Connector::readSocketWorker()
                             // Read Output State 0 to 7 -> robotState.getIO(8..15)
                             for (int i=0; i<8; i++)
                             {
-                                robotState.set_IO(7+i, masterBoardData->bit_to_bool(masterBoardData->DigitaOutputBits, i));
-                                ROS_DEBUG("Output State %d: %d", i, robotState.get_IO(7+i));
+                                robotState.set_IO(18+i, masterBoardData->bit_to_bool(masterBoardData->DigitaOutputBits, i));
++                               ROS_DEBUG("Output State %d: %d", 18 + i, robotState.get_IO(18+i));
                             }
 
                             // TODO: Read Tool inputs and outputs??? The following worked in the past but not with CB3.2

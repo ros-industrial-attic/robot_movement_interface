754449833c2458c1e5bf49fe4d34e2fb11977cd1
Witalij Siebert <witalij.siebert@freenet.de>
2016-06-13 14:09:07 +0200

    prototype off a gripper publisher

--- ur_driver/scripts/egl90_sim_publisher.py
+++ ur_driver/scripts/egl90_sim_publisher.py
@@ -2,23 +2,49 @@
 # license removed for brevity
 import rospy
 import math
+import actionlib
+
+from ur_driver.msg import *
 from sensor_msgs.msg import JointState
 
+# publishing rate of the gripper state
+publishFrequency = 50
+
+# the client
+client = actionlib.SimpleActionClient('digital_io_array', DigIOArrayAction)
 
 def talker():
+    # initialize gripper state publisher
     pub = rospy.Publisher('joint_states', JointState, queue_size=10)
-    rospy.init_node('egl90_sim_publisher', anonymous=True)
-    rate = rospy.Rate(50)
+    rate = rospy.Rate(publishFrequency)
 
-    checkIOs()
+    # this is the service where we are listening too for the inputs
+    #client = actionlib.SimpleActionClient('digital_io_array', DigIOArrayAction)
+    print "Waiting for 'DigitalIO' server."
+
+    # wait for the action until found
+    client.wait_for_server();
+    print "'DigitalIO' server found."    
+    
+    counter = 0
+    gripperOpen = False
+
+    # cyclic gripper data update
     while not rospy.is_shutdown():
 
         # publish gripper position
         msg = JointState()
         msg.header.stamp = rospy.Time.now()
         msg.name = ["egl_position"]
-        msg.position = [0.00] # 0.05 completly open # 0.0 closed
+        msg.position = [0.011] # 0.05 open | 0.011 closed
         pub.publish(msg)
+
+        if counter == int(publishFrequency):            
+            counter = 0            
+            checkIOs()
+            print client.get_result().state
+
+        counter = counter + 1
         rate.sleep()
 
 ### Check the IOs in a loop. Conditions for a closed grippe are the following:
@@ -26,32 +52,20 @@ def talker():
 # IONr. 23: True  && IONr. 24: False -- Open the gripper
 def checkIOs():
     ioNums = []
-    states = []
 
     # Queue IO 23 and IO 24
     # because we want to read only, it does not matter which state we are sending
     ioNums.append(23)
-    states.append(True)
+    ioNums.append(24)    
 
-    ioNums.append(24)
-    states.append(True)
+    goal = ur_driver.msg.DigIOArrayGoal(ioNr=ioNums, newState=[True] * len(ioNums), readOnly=[True] * len(ioNums))
 
-    actionMessage = {
-        'ioNr': ioNums,
-        'newState': states, 
-        'readOnly': [True] * len(ioNums)
-    }
-
-    print actionMessage
-
-    data = {
-        'actionID': '/digital_io_array',
-        'resultType': 'ur_driver/DigIOArrayActionResult',
-        'actionMessage': actionMessage
-    }
+    client.send_goal(goal)
+    client.wait_for_result()
 
 if __name__ == '__main__':
     try:
+        rospy.init_node('egl90_sim_publisher', anonymous=True)
         talker()
     except rospy.ROSInterruptException:
-        pass
+        print "Gripper simulation state publisher interrupted."
